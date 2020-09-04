adf101aa0ffa3cb1ed78287abc3e0c84c767b023
foskerbr <58860932+foskerbr@users.noreply.github.com>
2020-09-04 11:43:37 -0300

    Finalizando Responsivo

--- js/script.js
+++ js/script.js
@@ -10,6 +10,15 @@ $(document).ready(function() {
         }
 
     });
+    $("#btn-bars").on("click", function() {
+        $("header").toggleClass("open-menu");
+        $("#menu-mobile, #menu-mobile-mask").removeClass("hidden-menu");
+    });
+    $("#menu-mobile-mask, .btn-close").on("click", function() {
+        $("header").removeClass("open-menu");
+        $("#menu-mobile, #menu-mobile-mask").toggleClass("hidden-menu");
+    });
     $("#telefone").mask("(99) 9 9999-9999").replace(/\D/g, '');
-    
+
+
 });
